8d4f5a6bf1ab7562a6a163642c6286a6f7039b9a
thaihung110 <Hung.VT226046@sis.hust.edu.vn>
2024-03-25 14:06:29 +0700

    Add Lab2

---
+++ Lab02/Cart.java
@@ -0,0 +1,49 @@
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc[] itemsOrdered = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	private int qtyOrdered = 0;
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (this.qtyOrdered < MAX_NUMBERS_ORDERED) {
+			this.itemsOrdered[this.qtyOrdered] = disc;
+			this.qtyOrdered++;
+			System.out.println("The disc has been added");
+		}
+		else {
+			System.out.println("The cart is almost full");
+		}
+	}
+	
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		int flag = 0;
+		int idx = 0;
+		for (int i = 0; i < this.qtyOrdered; i++) {
+			if (disc.equals(itemsOrdered[i])) {
+				flag = 1;
+				idx = i;
+				break;
+			}
+		}
+		if (flag == 0) {
+			System.out.println("Cannot remove");
+		}
+		else {
+			for (int i = idx; i <= this.qtyOrdered - 2; i++) {
+				this.itemsOrdered[i] = this.itemsOrdered[i + 1];
+			}
+			this.qtyOrdered--;
+			System.out.println("Removed sucessfully");
+		}
+		
+	}
+	
+	public float totalCost() {
+		float s = 0.0f;
+		for (int i = 0; i < this.qtyOrdered; i++) {
+			s += this.itemsOrdered[i].getCost();
+		}
+		
+		return s;
+	}
+}
---
+++ Lab02/DigitalVideoDisc.java
@@ -0,0 +1,62 @@
+import java.util.Objects;
+
+public class DigitalVideoDisc {
+	private String title;
+	private String category;
+	private String director;
+	private int length;
+	private float cost;
+	
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+	}
+	
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+	
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DigitalVideoDisc other = (DigitalVideoDisc) obj;
+		return Objects.equals(category, other.category)
+				&& Float.floatToIntBits(cost) == Float.floatToIntBits(other.cost)
+				&& Objects.equals(director, other.director) && length == other.length
+				&& Objects.equals(title, other.title);
+	}
+}
