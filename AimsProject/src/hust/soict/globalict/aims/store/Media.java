531a9ae5a2cfac7dfb657a4231db264c9552b020
thaihung110 <Hung.VT226046@sis.hust.edu.vn>
2024-04-25 01:27:05 +0700

    Adding Lab03

--- AimsProject/src/hust/soict/globalict/aims/store/Media.java
+++ AimsProject/src/hust/soict/globalict/aims/store/Media.java
@@ -7,7 +7,6 @@ public abstract class Media {
 	public static final Comparator<Media> COMPARE_BY_TITLE_COST = new MediaComparatorByTitleCost();
 	public static final Comparator<Media> COMPARE_BY_COST_TITLE = new MediaComparatorByCostTitle();
 	
-
 	private int id;
 	private String title;
 	private String category;
